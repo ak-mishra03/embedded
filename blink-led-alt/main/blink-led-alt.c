efb37c184f8401e4691a7e40e656da43288503a0
ak-mishra03 <143195815+ak-mishra03@users.noreply.github.com>
2026-07-25 03:43:57 +0530

    made the code reusable and added several patterns for two independent LEDs

--- blink-led-alt/main/blink-led-alt.c
+++ blink-led-alt/main/blink-led-alt.c
@@ -1,35 +1,118 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
-#include "esp_log.h"
 
-#define LED_PIN_1 GPIO_NUM_23
-#define LED_PIN_2 GPIO_NUM_21
+#define NUM_LEDS 2
+
+gpio_num_t leds[NUM_LEDS] = {
+  GPIO_NUM_22,
+  GPIO_NUM_23
+};
+
+/* Play any pattern */
+void play_pattern(const int pattern[][NUM_LEDS], int rows, int delay_ms)
+{
+  for (int i = 0; i < rows; i++) {
+
+    for (int j = 0; j < NUM_LEDS; j++) {
+      gpio_set_level(leds[j], pattern[i][j]);
+    }
+
+    vTaskDelay(pdMS_TO_TICKS(delay_ms));
+  }
+}
 
 void app_main(void)
 {
-    gpio_reset_pin(LED_PIN_1);
-    gpio_reset_pin(LED_PIN_2);
+  /* Initialize LEDs */
+  for (int i = 0; i < NUM_LEDS; i++) {
+    gpio_reset_pin(leds[i]);
+    gpio_set_direction(leds[i], GPIO_MODE_OUTPUT);
+  }
 
-    gpio_set_direction(LED_PIN_1, GPIO_MODE_OUTPUT);
-    gpio_set_direction(LED_PIN_2, GPIO_MODE_OUTPUT);
+  /* ---------------- Police ---------------- */
+  const int police[][NUM_LEDS] = {
+    {1,0},
+    {0,1},
+    {1,0},
+    {0,1},
+    {1,0},
+    {0,1},
+  };
 
-    while (1) {
+  /* --------------- Blink Together --------------- */
+  const int blink[][NUM_LEDS] = {
+    {1,1},
+    {0,0},
+    {1,1},
+    {0,0},
+    {1,1},
+    {0,0},
+  };
 
-      // for led on gpio 23
-        ESP_LOGI("LED1", "ON");
-        gpio_set_level(LED_PIN_1, 1);
-        vTaskDelay(pdMS_TO_TICKS(200));
-        ESP_LOGI("LED1", "OFF");
-        gpio_set_level(LED_PIN_1, 0);
-        vTaskDelay(pdMS_TO_TICKS(200));
+  /* ---------------- Double Flash ---------------- */
+  const int double_flash[][NUM_LEDS] = {
+    {1,0},
+    {0,0},
+    {1,0},
+    {0,0},
 
-        // for led on gpio 21
-        ESP_LOGI("LED1", "ON");
-        gpio_set_level(LED_PIN_2, 1);
-        vTaskDelay(pdMS_TO_TICKS(200));
-        ESP_LOGI("LED1", "OFF");
-        gpio_set_level(LED_PIN_2, 0);
-        vTaskDelay(pdMS_TO_TICKS(200));
-    }
+    {0,1},
+    {0,0},
+    {0,1},
+    {0,0},
+  };
+
+  /* ---------------- Ping Pong ---------------- */
+  const int ping_pong[][NUM_LEDS] = {
+    {1,0},
+    {1,1},
+    {0,1},
+    {1,1},
+    {1,0},
+    {1,1},
+    {0,1},
+    {1,1},
+  };
+
+  /* ---------------- Binary Counter ---------------- */
+  const int binary[][NUM_LEDS] = {
+    {0,0},
+    {0,1},
+    {1,0},
+    {1,1},
+  };
+
+  while (1) {
+
+    play_pattern(police,
+        sizeof(police) / sizeof(police[0]),
+        300);
+
+    vTaskDelay(pdMS_TO_TICKS(700));
+
+    play_pattern(blink,
+        sizeof(blink) / sizeof(blink[0]),
+        400);
+
+    vTaskDelay(pdMS_TO_TICKS(700));
+
+    play_pattern(double_flash,
+        sizeof(double_flash) / sizeof(double_flash[0]),
+        200);
+
+    vTaskDelay(pdMS_TO_TICKS(700));
+
+    play_pattern(ping_pong,
+        sizeof(ping_pong) / sizeof(ping_pong[0]),
+        300);
+
+    vTaskDelay(pdMS_TO_TICKS(700));
+
+    play_pattern(binary,
+        sizeof(binary) / sizeof(binary[0]),
+        500);
+
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  }
 }
